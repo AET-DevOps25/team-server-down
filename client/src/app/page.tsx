9fd2b2b8c4d57f219b4a533ae57d50d2cd57bc12
Armanpreet Ghotra <arman.ghotra@tum.de>
2025-05-12 16:55:05 +0200

    Add initial whiteboard component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,19 @@
-import { Button } from "@/components/ui/button";
+import React from "react";
+import {
+  ReactFlow,
+  Background,
+  BackgroundVariant,
+  Controls,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
 export default function Home() {
-  return <Button>Button</Button>;
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow fitView>
+        <Controls position="bottom-right" />
+        <Background variant={BackgroundVariant.Dots} gap={16} size={1} />
+      </ReactFlow>
+    </div>
+  );
 }
