7bae2179fad26177fcdb0bdd726416ee8f97fb14
Leon Liang <hello@leonliang.lu>
2025-06-24 20:55:18 +0200

    add scopes

--- server/src/main/java/de/tum/cit/aet/devops/teamserverdown/config/OpenAPIConfiguration.java
+++ server/src/main/java/de/tum/cit/aet/devops/teamserverdown/config/OpenAPIConfiguration.java
@@ -1,21 +1,14 @@
 package de.tum.cit.aet.devops.teamserverdown.config;
 
-import de.tum.cit.aet.devops.teamserverdown.security.CurrentUser;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.*;
 import io.swagger.v3.oas.models.servers.Server;
-import java.util.Iterator;
-import java.util.List;
-import org.springdoc.core.customizers.OperationCustomizer;
+import java.util.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.MethodParameter;
-import org.springframework.web.method.HandlerMethod;
 
 @Configuration
 public class OpenAPIConfiguration {
@@ -23,6 +16,8 @@ public class OpenAPIConfiguration {
   @Bean
   public OpenAPI openAPI() {
     final String securitySchemeName = "keycloak";
+
+    Scopes scopes = new Scopes();
 
     SecurityScheme securityScheme =
         new SecurityScheme()
@@ -36,7 +31,8 @@ public class OpenAPIConfiguration {
                                 System.getenv("IDP_EXTERNAL_URI") + "/protocol/openid-connect/auth")
                             .tokenUrl(
                                 System.getenv("IDP_EXTERNAL_URI")
-                                    + "/protocol/openid-connect/token")));
+                                    + "/protocol/openid-connect/token")
+                            .scopes(scopes)));
 
     return new OpenAPI()
         .info(
@@ -51,33 +47,5 @@ public class OpenAPIConfiguration {
                 .url("https://github.com/AET-DevOps25/team-server-down"))
         .components(new Components().addSecuritySchemes(securitySchemeName, securityScheme))
         .addSecurityItem(new SecurityRequirement().addList(securitySchemeName));
-  }
-
-  @Bean
-  public OperationCustomizer hideCurrentUser() {
-    return (Operation operation, HandlerMethod handlerMethod) -> {
-      if (operation.getParameters() == null) {
-        return operation;
-      }
-
-      MethodParameter[] methodParameters = handlerMethod.getMethodParameters();
-      List<Parameter> openApiParams = operation.getParameters();
-
-      Iterator<Parameter> paramIterator = openApiParams.iterator();
-      while (paramIterator.hasNext()) {
-        Parameter openApiParam = paramIterator.next();
-        for (MethodParameter methodParam : methodParameters) {
-          if (methodParam.hasParameterAnnotation(CurrentUser.class)) {
-            String methodParamName = methodParam.getParameterName();
-            if (methodParamName == null || openApiParam.getName().equals(methodParamName)) {
-              paramIterator.remove();
-              break;
-            }
-          }
-        }
-      }
-
-      return operation;
-    };
   }
 }
