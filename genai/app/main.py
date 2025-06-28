2e3e149a5ba7d64005a85fb6526853718bb2197d
Armanpreet Ghotra <arman.ghotra@tum.de>
2025-06-28 15:50:12 +0200

    add prometheus, grafana, loki and promtrail

--- genai/app/main.py
+++ genai/app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.routes import root
 from app.db.client import WeaviateClientSingleton
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 @asynccontextmanager
@@ -12,5 +13,6 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+Instrumentator().instrument(app).expose(app)
 
 app.include_router(root.router)
